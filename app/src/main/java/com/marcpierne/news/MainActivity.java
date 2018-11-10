1e163db5e09ad0f717e154ab2bda6606aacdb987
Darkhammer12 <Darkhammer12@protonmail.com>
2018-11-10 12:09:18 -0800

    NewsStore and NewsArticle

--- app/src/main/java/com/marcpierne/news/MainActivity.java
+++ app/src/main/java/com/marcpierne/news/MainActivity.java
@@ -3,11 +3,25 @@ package com.marcpierne.news;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.marcpierne.news.model.NewsArticle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<NewsArticle> newsArticles = new ArrayList<>();
+        newsArticles.add(new NewsArticle("Doctors share gun stories, demand action after NRA tells them to 'stay in their lane","Two signs of the times: another mass shooting and more Twitter outrage. This time, though, the barbs over social media came from the medical community in response to a tweet from the National Rifle Association.",
+                "https://cdn.cnn.com/cnnnext/dam/assets/151008102129-gun-american-flag-illustration-exlarge-tease.jpg", "today,","https://www.cnn.com/2018/11/08/health/nra-tweet-doctors-react/index.html"));
+
+        newsArticles.add(new NewsArticle("Turkey shares tapes on Khashoggi's death", "Istanbul, Turkey (CNN)Recordings related to Saudi journalist Jamal Khashoggi's death have been passed on to Saudi Arabia, the United States, the United Kingdom, Germany and France, Turkey's President Recep Tayyip Erdogan said Saturday.",
+                "https://cdn.cnn.com/cnnnext/dam/assets/181003085839-01-jamal-khashoggi-file-medium-tease.jpg", "today", "https://www.cnn.com/2018/11/10/middleeast/turkey-saudi-khashoggi-investigation-intl/index.html"));
+
+        NewsStore.setNewsArticles(newsArticles);
     }
 }
