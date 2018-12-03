f2212117199da054574f7e4cea6585b45b842355
Darkhammer12 <Darkhammer12@protonmail.com>
2018-12-02 21:50:08 -0800

    added unit tests for Date Utils with firebase analytics

--- app/src/main/java/com/marcpierne/news/MainActivity.java
+++ app/src/main/java/com/marcpierne/news/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 /* import com.marcpierne.news.model.NewsArticle;// */
@@ -25,21 +26,24 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView newsRecyclerView;
     private CoordinatorLayout coordinatorLayout;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        newsRecyclerView = (RecyclerView) findViewById(R.id.activity_main);
+        newsRecyclerView = (RecyclerView) findViewById(R.id.activity_main_recyclerview);
         newsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.activity_main_progressbar);
 
-        Call<GetArticlesResponse> call = NewsAPI.getApi().getArticles("reuters", "top");
+        Call<GetArticlesResponse> call = NewsAPI.getApi().getArticles("espn", "top");
         call.enqueue(new Callback<GetArticlesResponse>() {
             @Override
             public void onResponse(Call<GetArticlesResponse> call, Response<GetArticlesResponse> response) {
+                progressBar.setVisibility(View.GONE);
                 showNewsApiSnack();
                 GetArticlesResponse getArticlesResponse = response.body();
                 NewsStore.setNewsArticles(getArticlesResponse.getArticles());
@@ -50,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<GetArticlesResponse> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
                 Toast.makeText(MainActivity.this, "Error received", Toast.LENGTH_SHORT).show();
             }
         });
--- app/src/test/java/com/marcpierne/news/utils/DateUtilsTest.java
+++ app/src/test/java/com/marcpierne/news/utils/DateUtilsTest.java
@@ -1,7 +1,15 @@
 package com.marcpierne.news.utils;
 
 
+import com.google.firebase.crash.FirebaseCrash;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import java.text.ParseException;
 
 
 import static org.junit.Assert.assertEquals;
@@ -10,9 +18,13 @@ import static org.junit.Assert.assertEquals;
 
 
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({FirebaseCrash.class})
 public class DateUtilsTest {
     String correctInputDate1 = "2016-07-25T09:56:27Z";
     String correctOutputDate1 = "Mon, 25 Jul 2016 09:56";
+
+    String incorrectInputDate1 = "2016-07-25T09";
 
     @Test
     public void formatNewsApiDate_correctDate_outputsCorrectDate() {
@@ -24,5 +36,16 @@ public class DateUtilsTest {
     public void formatNewsApiDate_nullInput_outputsNull() {
         String outputDate = DateUtils.formatNewsApiDate(null);
         assertEquals(outputDate, null);
+    }
+
+    @Test
+    public void formatNewsApiDate_incorrectInput_returnsSame() {
+        PowerMockito.mockStatic(FirebaseCrash.class);
+
+        String outputDate = DateUtils.formatNewsApiDate(incorrectInputDate1);
+
+        assertEquals(outputDate, incorrectInputDate1);
+        PowerMockito.verifyStatic();
+        FirebaseCrash.report(Matchers.isA(ParseException.class));
     }
 }
